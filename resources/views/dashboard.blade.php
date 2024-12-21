1adb9edb064dfd50397771759a7dc09df0b0736b
Kai <137057698+KaiIsAvailable@users.noreply.github.com>
2024-12-21 16:55:41 +0800

    Describe the changes you made

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,14 @@
 
 @section('content')
     <div class="container">
-        <h1>Dashboard</h1>
+        <h1><strong>Dashboard</strong></h1>
+        @auth
+            <div>Welcome {{ Auth::user()->name }} to Tham's Taekwon-do Academy</div>
+        @else
+            <div>Welcome to Tham's Taekwon-do Academy</div>
+        @endauth
+        <br>
         <p>Total number of students: {{ $studentCount }}</p>
+        
     </div>
 @endsection
